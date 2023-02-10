813ef9a19eddbeda5b0dda770ede99f2f3a45353
populus <emil.a.johansson@gmail.com>
2023-02-10 05:14:30 +0100

    Drafted models for upcoming features. (Posts, Events, Forums).

---
+++ Models/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace CommunityWebsite_Lexicon_Project.Models
+{
+    public class Event : Post
+    {
+        public string? EventId { get; set; }
+        public DateTime HighlightedDateTime { get; set; }
+        public List<Account>? UsersAttending { get; set; }
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace CommunityWebsite_Lexicon_Project.Models
+{
+    public class Post
+    {
+        public string? PostId { get; set; }
+        public string? PostTitle { get; set; }
+        public DateTime PostCreationDateTime { get; set; }
+        public List<string>? Tags { get; set; }
+        public List<string>? AttachedImages { get; set; }
+        public string? PostMessage { get; set; }
+    }
+}
